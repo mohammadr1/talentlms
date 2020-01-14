8b215629f2b1e4043cf3e0310a12f6e7dff47fa7
mohammadr1 <mahdir2302@gmail.com>
2020-01-14 12:02:50 -0800

    و تغییرات بخش روت ها User info اضافه کردن بخش

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
 import React from 'react';
 // import "bootstrap/dist/css/bootstrap.min.css";
 import Header from './components/sections/Header';
-import { Route, Link } from 'react-router-dom';
-import Home from './components/Home';
-import About from './components/About';
-import Contact from './components/Contact';
-import CreateCourse from './components/CreateCourse';
-import Course from './components/Course';
+// import { Route, Link } from 'react-router-dom';
+import { Switch, Route } from "react-router-dom"; 
+import routes from "../src/Routes";
 
 function App() {
   return (
    <div>
       <Header />
-      <Route path="/" component={Home} exact={true}/>
-      <Route path="/About" component={About} />
-      <Route path="/contact" component={Contact} />
-      <Route path="/CreateCourse" component={CreateCourse} />
-      <Route path="/Course" component={Course} />
+      <Switch>
+        { routes.map( route => <Route key={ route.path } { ...route } /> ) }
+      </Switch>
     </div>
   );
 }
